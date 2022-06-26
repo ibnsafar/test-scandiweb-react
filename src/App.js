336b7eaed76b39198983b7f9fd1ec7290b21640b
ibnsafar <yokubibnsafar@gmail.com>
2022-06-26 10:23:22 +0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 class App extends React.Component {
+
+    componentDidMount() {
+        console.log("component did mount")
+    }
 
     render() {
         return (
